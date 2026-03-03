ed7edcb3e2384a0cc9bf809dee76c54210770692
Yassine Ben kacem <yassinbenkacem12@gmail.com>
2026-03-03 14:45:25 +0000

    first commit

---
+++ advanced_mcp_server.py
@@ -0,0 +1,46 @@
+from fastmcp import FastMCP
+from pathlib import Path
+import logging
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+logging.getLogger("fastmcp").setLevel(logging.WARNING)
+
+mcp = FastMCP("HTTP File server.")
+
+"""
+    The top-level directories 
+    that the server is allowed to access.
+"""
+BASE_DIR = Path(__file__).parent / "workspace"
+BASE_DIR.mkdir(exist_ok=True)
+
+
+def is_within_roots(path:Path)->bool:
+    """
+        check if the path within allowed roots directory.
+    """
+    try:
+        path.resolve().relative_to(BASE_DIR.resolve())
+        return True
+    except Exception as e:
+        return False
+
+
+@mcp.tool(name="read_file", description="this tool helps to read an existing file")
+def read_file(file_path:Path):
+    path = BASE_DIR /  file_path
+    if not is_within_roots(path):
+        return f"Error: Access denied - path outside workspace roots"
+    
+    elif not path.exists():
+        return f"Error : the path proived doesn't exist." 
+    
+    else:
+        try:
+            content = path.read_text()
+            return content
+        except Exception as e:
+            return f"Error: {str(e)}"
+    
+
